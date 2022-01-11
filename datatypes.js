70908ea021a154972eb567b71f0de34020bf74c2
Laura <laura.e.dodds@gmail.com>
2022-01-11 19:02:44 +0200

    lesson 5 updates

--- datatypes.js
+++ datatypes.js
@@ -20,7 +20,8 @@ h2.innerHTML = `${day}, ${hours}:${minutes}`;
 
 //display forecast
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -49,6 +50,14 @@ class="emoji"
 
 //display weather
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKEY = "b258b70ded4754fdc5255e1806dbf713";
+  let apiURl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKEY}&units=metric`;
+
+  axios.get(apiURl).then(displayForecast);
+}
+
 function showTemp(response) {
   console.log(response.data);
   let temp = Math.round(response.data.main.temp);
@@ -71,6 +80,8 @@ function showTemp(response) {
   );
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 //currentlocation
@@ -121,5 +132,3 @@ farenheitlink.addEventListener("click", showFarenheittemp);
 
 let celsiuslink = document.querySelector("#celsius");
 celsiuslink.addEventListener("click", showCelsiustemp);
-
-displayForecast();
